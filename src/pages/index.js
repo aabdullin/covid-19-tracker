a9c0a24c45fbc70cd244b97a921c46c1814f9072
aabdullin <rutrika@gmail.com>
2020-10-07 08:58:22 -0700

    wip (lint errors)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React from "react"
+import React from 'react'
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
+    <div style={{ color: 'purple' }}>
       <h1>Hello Gatsby!</h1>
       <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <img src='https://source.unsplash.com/random/400x200' />
     </div>
-  )
+  );
 }
