a9a47e3d95eb6eda3fa1b59d57278a492b109502
aabdullin <rutrika@gmail.com>
2020-10-16 08:41:37 -0700

    wip (lint errors)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const Home = () => {
-  return (
-    <div style={{ color: 'purple' }}>
-      <h1>Hello Gatsby!</h1>
-      <p>What a world.</p>
-      <img src='https://source.unsplash.com/random/400x200' />
-    </div>
-  );
-};
+const Home = () => (
+  <div style={{ color: 'purple' }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src='https://source.unsplash.com/random/400x200' />
+  </div>
+);
 export default Home;
