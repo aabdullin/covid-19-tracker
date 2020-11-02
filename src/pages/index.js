21a8f683c8e428de32380bd452e305158ea97131
Artur <arthur@bevager.com>
2020-11-01 18:37:19 -0800

    lint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import '../bootstrap-4.5.3-dist/css/bootstrap.css';
 
-
 const Home = () => (
   <div style={{ color: 'purple' }}>
-    <h1 class="text-success">Hello Gatsby!</h1>
-    <p class="text-muted">What a world.</p>
+    <h1 className='text-success'>Hello Gatsby!</h1>
+    <p className='text-muted'>What a world.</p>
     <img src='https://source.unsplash.com/random/400x200' />
   </div>
 );
