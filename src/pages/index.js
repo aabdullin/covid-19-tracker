9f3d27d3b695363c4e7bd18e29e306ca556a0bb0
Artur <arthur@bevager.com>
2020-11-01 19:13:14 -0800

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Home = () => (
-  <div style={{ color: 'purple' }}>
-    <h1 className='text-success'>Hello Gatsby!</h1>
-    <p className='text-muted'>What a world.</p>
-    <img src='https://source.unsplash.com/random/400x200' />
+  <main class="container">
+  <div style={{ color: 'black' }}>
+    <h1>COVID-19 Tracker</h1>
+    <a className='text-muted' href="https://github.com/mathdroid/covid-19-api">Data via Mathdroid's COVID-19 API</a>
   </div>
+  </main>
 );
 export default Home;
