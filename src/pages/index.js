d79e62ada134f3b7eb7db2c44b7a9d54887e32f0
Artur <arthur@bevager.com>
2020-11-01 19:57:09 -0800

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const Home = () => (
-  <main class="container">
-  <div style={{ color: 'black' }}>
-    <h1>COVID-19 Tracker</h1>
-    <a className='text-muted' href="https://github.com/mathdroid/covid-19-api">Data via Mathdroid's COVID-19 API</a>
-  </div>
-  </main>
-);
-export default Home;
+
+function App() {
+  const [{ data: getData, loading: getLoading, error: getError }] = useAxios(
+    'https://reqres.in/api/users/1'
+  )
+
+  return (
+    <main class="container">
+    <div id="root">
+      <h1>COVID-19 Tracker</h1>
+      <a className='text-muted' href="https://github.com/mathdroid/covid-19-api">Data via Mathdroid's COVID-19 API</a>
+    </div>
+    <div id="global_cases">
+      <h1>Global Cases</h1>
+    </div>
+    </main>
+  );
+}
+export default App
