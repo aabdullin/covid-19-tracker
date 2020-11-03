914186ec6d068a0e503ff418c4458e02bbf48b98
Artur <rutrika@gmail.com>
2020-11-02 21:12:32 -0800

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import useAxios from 'axios-hooks'
-import '../bootstrap-4.5.3-dist/css/bootstrap.css';
-
 
 // const [{ confirmed, loading, error }, refetch] = useAxios(
 //     'https://covid19.mathdro.id/api/confirmed'
@@ -13,25 +11,24 @@ class App extends React.Component {
         return (
             <main class="container">
                 <div id="root">
-                <h1>COVID-19 Tracker</h1>
-                <a className='text-muted' href="https://github.com/mathdroid/covid-19-api">Data via Mathdroid's COVID-19 API</a>
-                </div>
-                <div id="global_cases">
-                <h1>Global Cases</h1>
-                </div>
-                <div id="cases_apis">
-                <div id="cases_confirmed">
-                    <button onClick={refetch}>refetch</button>
-                    <a>Confirmed:</a>
-                    <pre>{JSON.stringify(confirmed, null, 2)}</pre>
-                </div>
-                <div id="cases_recovered">
-                    <h1>Recovered:</h1>
-                </div>
-                <div id="deaths">
-                    <h1>Deaths:</h1>
-                </div>
-
+                    <h1>COVID-19 Tracker</h1>
+                    <a className='text-muted' href="https://github.com/mathdroid/covid-19-api">Data via Mathdroid's COVID-19 API</a>
+                    </div>
+                    <div id="global_cases">
+                    <h1>Global Cases</h1>
+                    </div>
+                    <div id="cases_apis">
+                    <div id="cases_confirmed">
+                        <button onClick={refetch}>refetch</button>
+                        <a>Confirmed:</a>
+                        <pre>{JSON.stringify(confirmed, null, 2)}</pre>
+                    </div>
+                    <div id="cases_recovered">
+                        <h1>Recovered:</h1>
+                    </div>
+                    <div id="deaths">
+                        <h1>Deaths:</h1>
+                    </div>
                 </div> 
             </main>
         )
