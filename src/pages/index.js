d1f6b4d64a01a6a8cc2915603f2d3f976a0b2c03
Artur <rutrika@gmail.com>
2020-11-02 21:12:32 -0800

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-  </Layout>
-)
 
-export default IndexPage
+const Home = () => (
+  <div style={{ color: 'purple' }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <h1 class="text-success">Hello Gatsby!</h1>
+    <p class="text-muted">What a world.</p>
+    <img src='https://source.unsplash.com/random/400x200' />
+  </div>
+);
+
+// ReactDOM.render(
+//   <App />, document.querySelector('#root')
+// )
+
