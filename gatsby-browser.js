3467d4709f61c0af47baa2c074574279cf0ec4c8
Artur <rutrika@gmail.com>
2020-11-02 19:54:40 -0800

    wip

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 import 'jquery/dist/jquery.min.js';
 import '/node_modules/@popperjs/core/dist/popper.min';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min';
-import 'popper.js/dist/popper.min'
+import 'popper.js/dist/popper.min';
 import 'bootstrap/dist/js/bootstrap.min';
 
 const Home = () => (
