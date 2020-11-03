ddcfd5ca874d09f98a0ec0ae849abc249937c115
Artur <rutrika@gmail.com>
2020-11-02 20:19:33 -0800

    wip

--- src/components/ConfirmedCard.js
+++ src/components/ConfirmedCard.js
@@ -4,9 +4,7 @@ class ConfirmedCard extends React.Component {
   render() {
     return (
       <div id='cases_confirmed'>
-        <button onClick={refetch}>refetch</button>
         <a>Confirmed:</a>
-        <pre>{JSON.stringify(confirmed, null, 2)}</pre>
       </div>
     );
   }
