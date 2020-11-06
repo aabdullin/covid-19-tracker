eca9ac6a24183c7ea2d2a356e009f8509b255161
Artur <rutrika@gmail.com>
2020-11-06 09:14:37 -0800

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Cards from './Cards';
+import Cards from '../components/Cards';
 
 const Index = () => (
-  <main className='container'>
+  <main id='container'>
     <div id='root'>
       <h1>COVID-19 Tracker</h1>
-      <a className='text-muted' href='https://github.com/mathdroid/covid-19-api'>Data via Mathdroids COVID-19 API</a>
+      <a id='text-muted' href='https://github.com/mathdroid/covid-19-api'>Data via Mathdroids COVID-19 API</a>
     </div>
     <div id='global_cases'>
       <h1>Global Cases</h1>
