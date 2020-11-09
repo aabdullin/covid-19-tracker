38d6558aeab9a21fd35793675a3e3c4cd7df3cd7
Artur <14880970+aabdullin@users.noreply.github.com>
2020-11-09 09:13:20 -0800

    Task 4 moving logo (#18)
    * wip
    
    * lint fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Cards from '../components/Cards';
+import Rotate from '../components/RotateImage';
 
 const Index = () => (
   <main id='container'>
     <div id='root'>
+
+      <Rotate> <img src='https://cdn2.iconfinder.com/data/icons/medical-5-5/128/medical_radioactivity-256.png' width='50' height='50' /> </Rotate>
       <h1>COVID-19 Tracker</h1>
       <a id='text-muted' href='https://github.com/mathdroid/covid-19-api'>Data via Mathdroids COVID-19 API</a>
     </div>
