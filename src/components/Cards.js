8ec753dbd1dc1f0bbd8cdda96354f2273788fc43
Artur <rutrika@gmail.com>
2020-11-09 09:16:37 -0800

    Task 4. additional

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,9 +3,6 @@ import useAxios from 'axios-hooks';
 
 const Cards = () => {
   const [{ data, loading, error }] = useAxios('https://covid19.mathdro.id/api');
-  // const confirmedVal = JSON.stringify(data.confirmed.value, null, 2);
-  // const recoveredVal = JSON.stringify(data.recovered.value, null, 2);
-  // const deathVal = JSON.stringify(data.deaths.value, null, 2);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error!</p>;
@@ -23,7 +20,3 @@ const Cards = () => {
 };
 
 export default Cards;
-
-// <Card value={confirmedVal} class='badge badge-warning' name='Confirmed'> </Card>
-// <Card value={recoveredVal} class='badge badge-success' name='Recovered'> </Card>
-// <Card value={deathVal} class='badge badge-danger' name='Deaths'> </Card>
